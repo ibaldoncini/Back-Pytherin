b10f6350ec57b77fd97dc8575ddedf9ef27ffddb
Ignacio Baldoncini <nachobaldoncini@gmail.com>
2020-10-23 20:12:38 -0300

    Add check email util

---
+++ src/api/utils/room_utils.py
@@ -0,0 +1,12 @@
+from pony.orm import db_session, select
+from api.models.base import db
+
+
+async def check_email_status(email: str):
+    with db_session:
+        try:
+            email_confirmed = db.get(
+                "select email_confirmed from DB_User where email = $email")
+            return email_confirmed
+        except BaseException:
+            return None
