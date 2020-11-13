170c7a6ae2c8d7b89beccda355e08a49dd1898d9
jramosss <jramostod@gmail.com>
2020-11-13 19:44:01 -0300

    test_display_rooms finished

--- src/test_display_rooms.py
+++ src/test_display_rooms.py
@@ -1,9 +1,6 @@
-from api.routers.hub_endpoints import get_rooms
-from api.routers.room_endpoints import *
 from fastapi.testclient import TestClient
 from test_main import test_app
 from random import randint
-from api.routers.room_endpoints import hub
 from test_setup import join,create as create_room,start_game,p
 
 client = TestClient(test_app)
@@ -35,27 +32,78 @@ def create_several_rooms (n):
     #TODO max players
     create_room(owner,room_name)
 
-#TODO test if the match still appearing even if it started
-#hint: that should not happen
-def test_main ():
-  #This prints the room created by test_setup
-  print(get_rooms())
-  N_ROOMS = 5
+
+def test_get_rooms_empty():
+  response =client.get(
+    "/rooms",
+    headers=owner
+  )
+  print(response.json().__str__())
+  assert response.status_code == 200
+  #assert response.json() == {"rooms_list" : []}
+
+
+N_ROOMS = 5
+
+
+def test_get_rooms_plenty():
   create_several_rooms(N_ROOMS)
-  rooms = get_rooms()
-  print("Rooms: " + rooms.__str__())
-  real_rooms = rooms.get("room_list")
+  response =client.get(
+    "/rooms",
+    headers=owner
+  )
+  print(response.json().__str__())
+  assert response.status_code == 200
+  assert response.json != {"rooms_list" : []}
+
+
+
+def test_get_rooms_non_empty():
+  rooms =client.get(
+    "/rooms",
+    headers=owner
+  )
+  real_rooms = rooms.json().get("room_list")
   room_names = []
   for room in real_rooms:
     room_names.append(room.get("name"))
-  
+
+  print(room_names)
+
   for i in range (0,N_ROOMS):
     n_players = randint(1,4)
     join_several_players(room_names[i],n_players)
 
-  print("Rooms with people " + get_rooms().__str__())
+  rooms = client.get(
+    "/rooms",
+    headers=owner
+  )
+
+  print(rooms.json().__str__())
+  assert rooms.status_code == 200
+
+
+def test_start_game():
+  rooms =client.get(
+    "/rooms",
+    headers=owner
+  )
+  real_rooms = rooms.json().get("room_list")
+  room_names = []
+  for room in real_rooms:
+    room_names.append(room.get("name"))
 
   start_game(owner,room_names[0])
-  print("\n\nStarted first room: " + get_rooms().__str__())
+  rooms = client.get(
+    "/rooms",
+    headers=owner
+  )
+  rooms_json = rooms.json()
+  print("\n\nStarted first room: " + rooms_json.__str__())
+  #assert rooms_json
 
-#main()
+
+test_get_rooms_empty()
+test_get_rooms_plenty()
+test_get_rooms_non_empty()
+test_start_game()
