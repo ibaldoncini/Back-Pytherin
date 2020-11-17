504a759d3c6e46dd27b32e402df5640c40997f3f
nazagara <nazagara1277@gmail.com>
2020-11-17 09:02:29 -0300

    Fix votes thing

--- src/api/routers/room_endpoints.py
+++ src/api/routers/room_endpoints.py
@@ -315,7 +315,7 @@ async def vote(
         if vote_req.vote not in [Vote.LUMOS.value, Vote.NOX.value]:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid vote")
-        elif username in game.get_votes().keys():
+        elif username in game.get_votes(True).keys():
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail="You already voted")
         else:
